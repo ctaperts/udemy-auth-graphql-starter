6c791eb0b86569bad2f30c30627626a5da1ebe4f
Colby Taperts <colbytaperts@gmail.com>
2018-09-08 16:06:57 -0500

    finish up login form

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -6,19 +6,22 @@ class AuthForm extends Component {
 
     this.state = { email: '', password: '' };
   }
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state)
+  }
   render() {
     return (
       <div className="row">
-        <form className="col s6">
+        <form onSubmit={this.onSubmit.bind(this)} className="col s6">
           <div className="input-field">
-            <label>Email</label>
-            <input value={this.state.email}
+            <input placeholder="Email" value={this.state.email}
               onChange={e => this.setState({ email: e.target.value })}
             />
           </div>
           <div className="input-field">
-            <label>Password</label>
-            <input value={this.state.password}
+            <input placeholder="Password" type="password" value={this.state.password}
               onChange={e => this.setState({ password: e.target.value })}
             />
           </div>
